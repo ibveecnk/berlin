0bba65192e2d97aadb601cf1a31a9e8a4f1f4dfe
Iven Beck <ivenbeck@outlook.de>
2021-04-21 17:45:35 +0200

    Add command to stop music playback

---
+++ src/commands/music/Stop.ts
@@ -0,0 +1,32 @@
+import { Main } from "./../../index";
+import { Command, CommandMessage, Description } from "@typeit/discord";
+import { EmbedGenerator } from "../../helpers/EmbedGenerator";
+
+export default abstract class Stop {
+  @Command()
+  @Description("Stops music playback")
+  async stop(command: CommandMessage) {
+    if (!command.member?.voice.channel) {
+      command.channel.send(
+        EmbedGenerator.errorEmbed("Error", "You are not in a voice channel.")
+      );
+      return;
+    }
+
+    if (Main.Queue.get(command.guild.id) == undefined) {
+      command.channel.send(
+        EmbedGenerator.errorEmbed(
+          "Error",
+          "There is no music being played back at the moment."
+        )
+      );
+      return;
+    }
+
+    console.log(Main.Queue.get(command.guild.id));
+
+    Main.Queue.get(command.guild.id).connection.dispatcher.end();
+    Main.Queue.get(command.guild.id).connection.disconnect();
+    Main.Queue.delete(command.guild.id);
+  }
+}
